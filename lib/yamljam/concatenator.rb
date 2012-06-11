d6e058ecf02b8b6a43830163f2fc09e826386136
Bill Abney <bill.abney@gmail.com>
2012-06-11 15:40:58 -0700

    ugly but it works. Next up: make a gem executable

--- lib/yamljam/concatenator.rb
+++ lib/yamljam/concatenator.rb
@@ -1,6 +1,7 @@
 module Yamljam
 	class Concatenator
 		def concatenate(input_files, output_file)
+      puts "input files: #{input_files.inspect}"
       output_hashmap = merge_files(input_files)
       write_output(output_hashmap, output_file)
     end
@@ -16,18 +17,18 @@ module Yamljam
 		end
 
     def merge_files(input_files)
-      input_files.map{|input_file| make_hashmap(input_file)}.reduce({}, &:merge)
+      {@namespace => input_files.map{|input_file| make_hashmap(input_file)}.reduce({}, &:merge)}
     end
 
     def make_hashmap(input_file)
       base_hashmap = YAML::load_file(input_file)
-      return {input_file => base_hashmap}
     end
 
     def jam(input_directory_path = nil, namespace)
       # input directory will contain #{namespace}.yml (output file, which will be blown away)
       # and a directory called #{namespace}. All files in the #{namespace} directory will be
       # assumed to be yaml input files if they end in .yml or .yaml, and ignored otherwise.
+      @namespace = namespace
       input_directory_path ||= Dir.getwd
       input_file_dir = File.join(input_directory_path, namespace)
       Dir.chdir(input_file_dir) do
